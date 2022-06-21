6f10b594c9a6612942be657ae31f8a070503a964
Bibik <bibik@cdu.ru>
2022-06-21 14:02:27 +0300

    boolean env value

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 const processEnv = process.env
 export const proxyEnv = new Proxy(processEnv, {
     get: (target: typeof processEnv, prop: string) => {
-        if (prop in target) {
-            return target[prop]
+        const targetProp = target[prop]
+        // if (prop in target) {}
+        if(!targetProp) throw new Error(`process.env not contain '${prop}'`)
+
+        if(targetProp === 'true') {
+            return true
+        } else if(targetProp === 'false') {
+            return false
         }
-        throw new Error(`process.env not contain '${prop}'`)
+        return targetProp
     },
 })
