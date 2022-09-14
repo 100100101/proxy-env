97936c6557253972b1841fbb71edc0c7198eb9a3
Bibik <bibik@cdu.ru>
2022-09-14 14:18:48 +0300

    parse value in type

--- index.ts
+++ index.ts
@@ -3,12 +3,39 @@ export const proxyEnv = new Proxy(processEnv, {
     get: (target: typeof processEnv, prop: string) => {
         const targetProp = target[prop]
         // if (prop in target) {}
-        if(!targetProp) throw new Error(`process.env not contain '${prop}'`)
+        if (!targetProp) throw new Error(`process.env not contain '${prop}'`)
 
-        if(targetProp === 'true') {
+        if (targetProp === 'true') {
             return true
-        } else if(targetProp === 'false') {
+        } else if (targetProp === 'false') {
             return false
+        }
+        const targetPropConvertedToString = targetProp.toString()
+        const targetPropConvertedToFloat = parseFloat(
+            targetPropConvertedToString
+        )
+        const targetPropConvertedToFloatString =
+            targetPropConvertedToFloat.toString()
+
+        if (
+            targetPropConvertedToFloat &&
+            targetPropConvertedToString.length ===
+                targetPropConvertedToFloatString.length
+        ) {
+            return targetPropConvertedToFloat
+        }
+
+        const targetPropConvertedToInt = parseInt(targetPropConvertedToString)
+        const targetPropConvertedToIntString =
+            targetPropConvertedToInt.toString()
+
+        if (
+            // targetPropConvertedToInt ??
+            (targetPropConvertedToInt || targetPropConvertedToInt === 0) &&
+            targetPropConvertedToString.length ===
+                targetPropConvertedToIntString.length
+        ) {
+            return targetPropConvertedToInt
         }
         return targetProp
     },
