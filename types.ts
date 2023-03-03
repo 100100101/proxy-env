83674f8d8fe75af07cd2d86e85bfa7a17c8985ac
JSerGit <bibik.serej@gmail.com>
2023-03-03 10:08:45 +0200

    step

--- types.ts
+++
@@ -1,5 +0,0 @@
-export type TEnvValue = boolean | number | string
-export type TGet = (target: typeof process.env, prop: string) => TEnvValue
-export type TProxyEnv = {
-    [key: string]: TEnvValue
-}
